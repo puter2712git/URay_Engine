02b004974a7956ddffc8da0c7f82e7a4f0d2d212
Dongjin Kuk <85764911+puter2712git@users.noreply.github.com>
2026-07-19 23:57:56 +0900

    Change glsl to hlsl

--- Shader/compile.sh
+++ Shader/compile.sh
@@ -1,2 +1,2 @@
-~/VulkanSDK/1.4.350.0/macOS/bin/glslc shader.vert -o vert.spv
-~/VulkanSDK/1.4.350.0/macOS/bin/glslc shader.frag -o frag.spv
+~/VulkanSDK/1.4.350.0/macOS/bin/dxc -spirv -T vs_6_0 -E VSMain shader.hlsl -Fo vert.spv
+~/VulkanSDK/1.4.350.0/macOS/bin/dxc -spirv -T ps_6_0 -E PSMain shader.hlsl -Fo frag.spv
---
+++ Shader/shader.hlsl
@@ -0,0 +1,48 @@
+struct ObjectConstants
+{
+    float4x4 model;
+};
+[[vk::push_constant]] ObjectConstants obj;
+
+struct FrameConstants
+{
+    float4x4 view;
+    float4x4 proj;
+};
+[[vk::binding(0)]] ConstantBuffer<FrameConstants> frame;
+
+struct VSInput
+{
+    [[vk::location(0)]] float3 inPosition : POSITION;
+    [[vk::location(1)]] float3 inColor : COLOR;
+};
+
+struct VSOutput
+{
+    float4 outPosition : SV_Position;
+    [[vk::location(0)]] float3 fragColor : TEXCOORD0;
+};
+
+struct PSOutput
+{
+    [[vk::location(0)]] float4 outColor : SV_Target;
+};
+
+VSOutput VSMain(VSInput input)
+{
+    VSOutput output;
+
+    output.outPosition = mul(frame.proj, mul(frame.view, mul(obj.model, float4(input.inPosition, 1.0))));
+    output.fragColor = input.inColor;
+
+    return output;
+}
+
+PSOutput PSMain(VSOutput input)
+{
+    PSOutput output;
+
+    output.outColor = float4(input.fragColor, 1.0);
+
+    return output;
+}
