b1652a97374e2daac4abc4433d5a003db0e35877
Dongjin Kuk <85764911+puter2712git@users.noreply.github.com>
2026-07-25 12:32:27 +0900

    Refactor render system (WIP)

---
+++ Source/Render/RenderDevice.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+namespace URay
+{
+
+class RenderDevice
+{
+public:
+    bool Initialize();
+    void Finalize();
+
+private:
+};
+
+} // namespace URay
