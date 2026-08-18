104802cd84469b23d985ab09af4863f43122d9c4
Dongjin Kuk <puter2712goo@gmail.com>
2026-08-18 21:26:18 +0900

    Add rect structure

---
+++ Source/Core/Math/Rect.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Core/Math/Vector2.h"
+
+namespace URay
+{
+
+struct Rect
+{
+    Vector2 position = Vector2::Zero;
+    Vector2 size = Vector2::Zero;
+};
+
+} // namespace URay
