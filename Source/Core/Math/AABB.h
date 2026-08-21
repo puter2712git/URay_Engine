e9f86fb846688d5cdb1281dd31168f3e7583bd4e
Dongjin Kuk <puter2712goo@gmail.com>
2026-08-21 21:16:44 +0900

    Add AABB methods

--- Source/Core/Math/AABB.h
+++ Source/Core/Math/AABB.h
@@ -9,6 +9,20 @@ struct AABB
 {
     Vector3 min = Vector3::Zero;
     Vector3 max = Vector3::Zero;
+
+    bool Intersects(const AABB& other) const
+    {
+        return min.x <= other.max.x && max.x >= other.min.x &&
+               min.y <= other.max.y && max.y >= other.min.y &&
+               min.z <= other.max.z && max.z >= other.min.z;
+    }
+
+    bool Contains(const AABB& other) const
+    {
+        return min.x <= other.min.x && other.max.x <= max.x &&
+               min.y <= other.min.y && other.max.y <= max.y &&
+               min.z <= other.min.z && other.max.z <= max.z;
+    }
 };
 
 } // namespace URay
